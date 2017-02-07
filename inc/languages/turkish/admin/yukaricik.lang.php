09f6ffd77145277f8d70f5daf2c8dabc1dfa0d8a
Hüseyin Körbalta <huseyinkorbalta@gmail.com>
2017-02-07 12:04:58 +0200

    Eklentinin Türkçe Dil Dosyası Eklendi.

---
+++ inc/languages/turkish/admin/yukaricik.lang.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Yukarı Çık v1.0 Eklentisinin Türkçe Dil Dosyası
+ * Eklenti Yapımcısı: Hüseyin KÖRBALTA (Simurg)
+ * Yapımcı Website: https://www.huseyinkorbalta.com
+ * Github Page: https://github.com/SimurgTR/MyBB-1.8.X-Yukari-Cik-Eklentisi-v1.0
+ * Versiyon: 1.0
+ */
+
+// Eklenti dil fonksiyonları
+
+$l['yukaricik_Eklentiadi'] = '<font color="#8e44ad"><strong>Yukarı Çık</strong></font>';
+$l['yukaricik_Aciklama'] = 'Forum sitenize sağ alt köşede yukarı çık butonu göstermenizi sağlar. Manuel olarak bu işlemi yapamayanlar için geliştirilmiş bir eklentidir.';
+$l['yukaricik_Website'] = 'https://github.com/SimurgTR/MyBB-1.8.X-Yukari-Cik-Eklentisi-v1.0';
+$l['yukaricik_Yapimci'] = 'Hüseyin KÖRBALTA (Simurg)';
+$l['yukaricik_Yapimcisite'] = 'https://www.huseyinkorbalta.com';
+$l['yukaricik_Versiyon'] = '1.0';
+$l['yukaricik_Surum'] = '18*';
+
+?>
